6692840c09d0d26379ca77d4cbd91699d228642c
AnaKyn-hub <74941756+AnaKyn-hub@users.noreply.github.com>
2022-06-01 21:47:00 -0300

    Implementing JavaScript

--- js/code.js
+++ js/code.js
@@ -1,13 +1,53 @@
-var val = document.getElementById("value");
-const div = document.getElementById("del");
+const del = document.getElementById("del");
+var opVerif = false;
 
 function deleted(){
-    div.style.animation = "";
-    setTimeout(() => div.style.animation = "delAnim 0.2s reverse");
+    del.style.animation = "";
+    setTimeout(() => del.style.animation = "delAnim 0.2s reverse");
+
+    document.getElementById('value').innerHTML = "";
 }
 
-function test(){
-    val.innerHTML = "1+1";
+function insert(num){
+    if(opVerif){
+        document.getElementById('value').innerHTML = "";
+        opVerif = false;
+    }
+
+    let val = document.getElementById("value").innerHTML;
+    document.getElementById('value').innerHTML = val + num;
 }
 
-test();
+function operations(symb){
+    const op = document.getElementById('value');
+    op.style.animation = "";
+    setTimeout(() => op.style.animation = "opAnim 0.2s");
+
+    opVerif = true;
+
+    switch (symb) {
+        case '+': 
+            document.getElementById('value').innerHTML + '+';
+            console.log("SOMOOOOOO");
+        break;
+
+        case '-':
+            document.getElementById('value').innerHTML + '-';
+            console.log("SUBTRAIIIIIIU");
+        break;
+
+        case '*':
+            document.getElementById('value').innerHTML + '*';
+            console.log("MULTIPLICOOOOOO");
+        break;
+
+        case '/':
+            document.getElementById('value').innerHTML + '/';
+            console.log("DIVIDIIIIIU");
+        break;
+    }
+}
+
+function resultado(){
+
+}
